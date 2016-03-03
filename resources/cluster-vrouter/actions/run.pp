2d5681b2c35b8f0701bd0b6f6604207a5dcfb8ff
Dmitry Shulyak <dshulyak@mirantis.com>
2016-03-03 12:56:30 +0200

    Remove all resources

--- resources/cluster-vrouter/actions/run.pp
+++
@@ -1,7 +0,0 @@
-notice('MODULAR: cluster-vrouter.pp')
-
-$network_scheme = hiera_hash('network_scheme', {})
-
-class { 'cluster::vrouter_ocf':
-  other_networks => direct_networks($network_scheme['endpoints']),
-}
--- resources/top-role-cinder-vmware/actions/run.pp
+++
@@ -1,9 +0,0 @@
-notice('MODULAR: cinder-vmware.pp')
-
-$cinder_hash      = hiera_hash('cinder_hash', {})
-
-if roles_include(['cinder-vmware']) {
-  $debug    = pick($cinder_hash['debug'], hiera('debug', true))
-  $volumes  = get_cinder_vmware_data($cinder_hash['instances'], $debug)
-  create_resources(vmware::cinder::vmdk, $volumes)
-}
